5424b82d28fd6bcbd0d8dc53dba7bf5e797ec72b
Mathieu CHOPLAIN <mathieu.choplain.etu@univ-lemans.fr>
2024-04-15 10:34:49 +0200

    lint: use teacher cfg + convert to LF

--- src/database.js
+++ src/database.js
@@ -6,13 +6,11 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE_NAME
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE_NAME
 });
 
-module.exports.getClient = () => {
-    return client;
-};
+module.exports.getClient = () => client;
--- src/server.js
+++ src/server.js
@@ -9,44 +9,44 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).send('hello!\n');
+  res.status(200).send('hello!\n');
 });
 
 /* Exception handlers */
 app.use((err, req, res, next) => {  //Handler for http-errors exceptions
-    if (!HTTPErrors.isHttpError(err)) {
-        return next(err);
-    }
+  if (!HTTPErrors.isHttpError(err)) {
+    return next(err);
+  }
 
-    console.error(`http-errors object: STATUS=${err.status} MESSAGE='${err.message}'`);
-    console.error(`Error stack: ${err.stack}`);
+  console.error(`http-errors object: STATUS=${err.status} MESSAGE='${err.message}'`);
+  console.error(`Error stack: ${err.stack}`);
 
-    res.status(err.status).json({
-        'message': err.message
-    });
+  res.status(err.status).json({
+    'message': err.message
+  });
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {  //Generic exception handler
-    console.error('UNHANDLED EXCEPTION:');
-    console.error(err.stack);
+  console.error('UNHANDLED EXCEPTION:');
+  console.error(err.stack);
 
-    res.status(500).json({
-        'message': 'An unknown server error occured'
-    });
+  res.status(500).json({
+    'message': 'An unknown server error occured'
+  });
 });
 
 /* Connect to database and start server */
 const databaseClient = require('./database').getClient();
 databaseClient.connect()
-    .then(() =>
-    {
-        app.listen(PORT, () => {
-            console.log(`Server listening on port ${PORT}`);
-        });
-    })
-    .catch((reason) =>
-    {
-        console.log('Failed to connect to database!');
-        console.log('Reason: ' + reason);
+  .then(() =>
+  {
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
     });
+  })
+  .catch((reason) =>
+  {
+    console.log('Failed to connect to database!');
+    console.log('Reason: ' + reason);
+  });
