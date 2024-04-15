60c7b5d8f3178626b9b237ddd7b85b2ff7a52f3a
Mathieu CHOPLAIN <mathieu.choplain.etu@univ-lemans.fr>
2024-04-15 09:51:44 +0200

    Initial commit

---
+++ src/database.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Database service
+ */
+const { Client } = require('pg');
+
+const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE_NAME
+});
+
+module.exports.getClient = () => {
+    return client;
+};
---
+++ src/server.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const PORT = process.env.PORT;
+
+const HTTPErrors = require('http-errors');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('hello!\n');
+});
+
+/* Exception handlers */
+app.use((err, req, res, next) => {  //Handler for http-errors exceptions
+    if (!HTTPErrors.isHttpError(err)) {
+        return next(err);
+    }
+
+    console.error(`http-errors object: STATUS=${err.status} MESSAGE='${err.message}'`);
+    console.error(`Error stack: ${err.stack}`);
+
+    res.status(err.status).json({
+        'message': err.message
+    });
+});
+
+app.use((err, req, res, next) => {  //Generic exception handler
+    console.error('UNHANDLED EXCEPTION:');
+    console.error(err.stack);
+
+    res.status(500).json({
+        'message': 'An unknown server error occured'
+    });
+});
+
+/* Connect to database and start server */
+const databaseClient = require('./database').getClient();
+databaseClient.connect()
+    .then(() =>
+    {
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    })
+    .catch((reason) =>
+    {
+        console.log('Failed to connect to database!');
+        console.log('Reason: ' + reason);
+    });
